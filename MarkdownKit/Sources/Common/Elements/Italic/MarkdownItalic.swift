4da46f0e4ed9e5922fe0325e3f143513777188d9
Vladyslava Kirichenko <dev.velga@gmail.com>
2019-11-21 10:00:36 +0100

    added support for bold+italic

--- MarkdownKit/Sources/Common/Elements/Italic/MarkdownItalic.swift
+++ MarkdownKit/Sources/Common/Elements/Italic/MarkdownItalic.swift
@@ -8,18 +8,34 @@
 import Foundation
 
 open class MarkdownItalic: MarkdownCommonElement {
-  
-  fileprivate static let regex = "(\\s|^)(\\*|_)(?![\\*_\\s])(.+?)(?<![\\*_\\s])(\\2)"
-  
-  open var font: MarkdownFont?
-  open var color: MarkdownColor?
-  
-  open var regex: String {
-    return MarkdownItalic.regex
-  }
-  
-  public init(font: MarkdownFont?, color: MarkdownColor? = nil) {
-    self.font = font?.italic()
-    self.color = color
-  }
+    fileprivate static let regex = "(\\s|^)(\\*|_)(?![\\*_\\s])(.+?)(?<![\\*_\\s])(\\2)"
+
+    open var font: MarkdownFont?
+    open var color: MarkdownColor?
+
+    open var regex: String {
+        return MarkdownItalic.regex
+    }
+
+    public init(font: MarkdownFont?, color: MarkdownColor? = nil) {
+        self.font = font?.italic()
+        self.color = color
+    }
+
+    public func match(_ match: NSTextCheckingResult, attributedString: NSMutableAttributedString) {
+        // deleting trailing markdown
+        attributedString.deleteCharacters(in: match.range(at: 4))
+        // formatting string (may alter the length)
+        // check if font is bold already:
+        let stringAttributes = attributedString.attributes(at: 0, longestEffectiveRange: nil, in: match.range(at: 3))
+        addAttributes(attributedString, range: match.range(at: 3))
+        if let font = font,
+            let stringFont = stringAttributes[.font] as? UIFont,
+            stringFont.fontDescriptor.symbolicTraits.contains(.traitBold),
+            let boldItalicFont = font.withTraits(.traitBold, .traitItalic) {
+            attributedString.addAttributes([NSAttributedString.Key.font: boldItalicFont], range: match.range(at: 3))
+        }
+        // deleting leading markdown
+        attributedString.deleteCharacters(in: match.range(at: 2))
+    }
 }
